991965cd33bb8c3e8a6e67ef16027de54dcf36c2
Jumabek <jumabek4044@gmail.com>
2019-11-20 16:45:34 +0900

    add 5 layer nn

--- neural_network.py
+++ neural_network.py
@@ -4,33 +4,76 @@ import torch.nn.functional as F
 import torchvision.transforms as transforms
 import torch.utils.data as utils
 from torch.utils.tensorboard import SummaryWriter
-import numpy as np
+from sklearn import metrics
+from os.path import join
 
 class Net(nn.Module):
 
-    def __init__(self,input_size,num_classes):
+    def __init__(self,input_size,num_classes,use_batchnorm=False):
         super(Net, self).__init__()
         # kernel
         self.input_size = input_size
         self.num_classes = num_classes
+        self.use_batchnorm = use_batchnorm
         self.build()
 
     def build(self):
 
-        self.fc1 = nn.Linear(self.input_size, 128)  
+        self.drop1 = nn.Dropout(p=0.1)
+        self.fc1 = nn.Linear(self.input_size, 128)
+        self.bn1 = nn.BatchNorm1d(num_features=128) 
+        self.drop2 = nn.Dropout(p=0.3) 
         self.fc2 = nn.Linear(128, 128)
+        self.bn2 = nn.BatchNorm1d(num_features=128)
         self.fc3 = nn.Linear(128, self.num_classes)
-        
 
+        
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        if self.use_batchnorm:
+            x = F.relu(self.drop1(self.bn1(self.fc1(x))))
+            x = F.relu(self.drop2(self.bn2(self.fc2(x))))
+        else:
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
+
+class Net5(nn.Module):
+
+    def __init__(self,input_size,num_classes,use_batchnorm=False):
+        super(Net, self).__init__()
+        # kernel
+        self.input_size = input_size
+        self.num_classes = num_classes
+        self.use_batchnorm = use_batchnorm
+        
+        layers = []
+        layers.append(nn.Linear(input_size,128))
+
+        layers.append(nn.BatchNorm1d(128))
+        layers.append(nn.Dropout(p=0.3))
+        layers.append(nn.Linear(128,256))
+        
+        layers.append(nn.BatchNorm1d(256))
+        layers.append(nn.Dropout(p=0.4))
+        layers.append(nn.Linear(256,256))
+        
+        layers.append(nn.BatchNorm1d(256))
+        layers.append(nn.Dropout(p=0.4))
+        layers.append(nn.Linear(256,128))
+
+        layers.append(nn.BatchNorm1d(128))
+        layers.append(nn.Dropout(p=0.3))        
+        layers.append(nn.Linear(128,num_classes))
+        self.model = nn.Sequential(*layers)
+        
+    def forward(self, x):
+        return self.model(x)
+
 class NetClassifier():
-    def __init__(self, input_size,num_classes,num_epochs=5,batch_size=100,lr=5e-3,reg=2.5e-2,runs_dir=None):
-        self.model = Net(input_size,num_classes)
+    def __init__(self, input_size,num_classes,num_epochs=5,batch_size=100,lr=5e-3,reg=2.5e-2,runs_dir=None,use_batchnorm=False):
+        self.model = Net(input_size,num_classes,use_batchnorm)
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.model = self.model.to(self.device)
 
@@ -53,7 +96,7 @@ class NetClassifier():
         dataset = utils.TensorDataset(tensor_x,tensor_y)
         train_loader = utils.DataLoader(dataset,batch_size=self.batch_size) 
         model = self.model
-
+        best_acc = -1
         for epoch in range(self.num_epochs):
             for i,(xi,yi) in enumerate(train_loader):
                 outputs = model(xi)
@@ -64,27 +107,50 @@ class NetClassifier():
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
-                if i%100==0 :
+                if i%50==0 :
                     pred = self.predict(x_val)
-                    acc = np.mean(pred==y_val)*100
+                    balanced_acc = metrics.balanced_accuracy_score(y_val,pred)*100
+                    if balanced_acc>best_acc:
+                        best_acc = balanced_acc
+                        self.save(model) 
+                    writer.add_scalar('Accuracy/Balanced Val',balanced_acc,seen_so_far)
+
+                    acc = metrics.accuracy_score(y_val,pred)*100
                     writer.add_scalar('Accuracy/Val',acc,seen_so_far)
+
                     if verbose:
                         print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
                                                .format(epoch+1, self.num_epochs, i+1, len(y)//self.batch_size, loss.item()))
         writer.close()
 
-    def predict(self,x):
+    def predict(self,x,eval_mode=False):
         tensor_x = torch.stack([torch.Tensor(i) for i in x]).to(self.device)
         bs = self.batch_size
         num_batch = x.shape[0]//bs +1*(x.shape[0]%bs!=0)
 
         pred = torch.zeros(0,dtype=torch.int64).to(self.device)
+        
+        if eval_mode:
+            model = self.load(self.model)
+        else:
+            model = self.model
+        model.eval()
+
         with torch.no_grad():
             for i in range(num_batch):
                 xi = tensor_x[i*bs:(i+1)*bs]
-                outputs = self.model(xi)
+                outputs = model(xi)
                 _, predi = torch.max(outputs.data,1)
                 pred = torch.cat((pred,predi))
         
         return pred.cpu().numpy()
 
+    def save(self,model):
+        path = join(self.runs_dir,'checkpoint.pth')
+        torch.save(model.state_dict(),path)
+
+    def load(self,model):
+        path = join(self.runs_dir,'checkpoint.pth')
+        model.load_state_dict(torch.load(path))
+        model.eval()
+        return model
